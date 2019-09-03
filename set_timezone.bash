fff5fc2f3bc47f43f4010d2d1b266e7d1664aa36
raim95 <raim9595@gmail.com>
2019-09-03 19:54:35 +0300

    v1.3.1

--- set_timezone.bash
+++ set_timezone.bash
@@ -1,4 +1,4 @@
-mtz = `timedatectl list-timezones |grep Moscow`
+mtz=`timedatectl list-timezones |grep Moscow`
 read -p "Your TimeZone - "$mtz", write?" commit
 case $commit in:
 y) sudo timedatectl set-timezone $mtz
